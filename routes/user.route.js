7380ee8a7d6a109c83b86fcc4de288fafa8f019d
= <=>
2019-01-24 22:33:30 +0700

    create mvc controller user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,37 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db');
+
 var shortid = require('shortid');
 
+var controller = require('../controllers/user.controller');
 
-router.get('/',function(req,res){
-  res.render('users/index',{users:db.get('users').value()});
-})
+router.get('/',controller.index)
 
 
-router.get('/search',function(req,res){
-  var q = req.query.q;
-  var filterUsers = db.get('users').value().filter(function(user){
-    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
-  
-  res.render('users/index',{users:filterUsers});
-});
+router.get('/search',controller.search);
 
-router.get('/create',function(req,res){
-  res.render('users/create');
-});
+router.get('/create',controller.create);
 
-router.post('/create',function(req,res){
-  req.body.id = shortid.generate();
-  db.get('users').push(req.body).write();
-  res.redirect('/users');
-});
+router.post('/create',controller.createPost);
 
-router.get('/:id',function(req,res){
-  var id = req.params.id;
-  var user = db.get('users').find({id:id}).value();
-  
-  res.render('users/view',{user:user});
-});
+router.get('/:id',controller.get);
 module.exports = router;
