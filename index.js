d266963d6de747d2a27aee4e6c12faf42aeec91c
= <=>
2019-01-26 17:03:28 +0700

    Signed Cookie

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views','views');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
+app.use(cookieParser('abcdksjagjdl12345'));
 app.use(express.static('public'));
 
 app.use('/users',userRoute);
