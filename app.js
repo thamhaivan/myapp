961e9eec368c1e7b248be487bfa1dfd34f161125
= <=>
2019-01-27 21:40:27 +0700

    branch mater app

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var app = require('express');
-var router = express.Router();
-console.log("JLGJDJG");
-app.set('view engine','views');
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var userRoute = require('./routes/user.route')
+
 
