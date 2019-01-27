859ccc05a7416dd92056883403fbb79846ffa394
daibbr <daibbr85@gmail.com>
2019-01-27 21:36:04 +0700

    branch home file app

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var app = require('express');
-var router = express.Router();
-console.log("JLGJDJG");
-app.set('view engine','views');
-console.log('asjgljdsg');
-console.log('asjgljdsg');
-console.log('asjgljdsg');
-console.log('asjgljdsg');
-console.log('asjgljdsg');
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route')
+var productRoute = require('./routes/product.route');
+var mongoose = require('mongoose');
 
